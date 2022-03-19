652fd86acc144d167eb2619629ef03302669d45b
Gift Nnko <nnkogift@hotmail.com>
2022-03-19 14:47:21 +0300

    :sparkles: setup(*): Initialization
     - Setup jest testing

--- src/new-hikvision.ts
+++ src/new-hikvision.ts
@@ -1,16 +1,44 @@
 import {EventEmitter} from "events";
 import {ConnectionOptions} from "./interfaces/connect";
+import {generateHeader, requestConnection, setupClientListeners} from "./services/hikvision";
 
 
-class Hikvision extends EventEmitter{
+export default class Hikvision extends EventEmitter {
     constructor() {
         super();
+        this._emitDataEvent = this._emitDataEvent.bind(this);
+        this._emitErrorEvent = this._emitErrorEvent.bind(this);
     }
 
-    connect(options: ConnectionOptions){
+    connected: boolean = false;
+    client: any;
+
+    _emitDataEvent(data: any) {
+        this.emit("data", data);
     }
 
-    disconnect(){
+    _emitErrorEvent(error: any) {
+        this.emit("error", error);
+    }
+
+    connect(options: ConnectionOptions) {
+        console.info(`Connecting to camera at ${options.host}:${options.port}...`);
+        const headers = generateHeader(options);
+        const client = requestConnection(options, headers);
+        this.connected = true;
+        this.client = client;
+        setupClientListeners(this.client, this._emitDataEvent, this._emitErrorEvent);
+        this.emit('connect', this);
+        console.info(`Connected to camera at ${options.host}:${options.port}!`);
+
+    }
+
+    disconnect() {
+        console.info(`Disconnecting from camera...`);
+
+        this.connected = false;
+        console.info(`Disconnected from camera`);
+        this.emit("disconnect")
     }
 
 
